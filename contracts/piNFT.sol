25b460bccd527c739e3dd3f2cd1ab19ebab7d71e
Parva Jain <parv@Parvas-MacBook-Air.local>
2022-06-28 18:12:07 +0530

    IERC20 added

--- contracts/piNFT.sol
+++ contracts/piNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "./IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
