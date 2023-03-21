e819b295608186e2c1d9666dd4811a50f14d2b8b
srsydv <shrish@iiitmanipur.ac.in>
2023-03-21 13:33:27 +0530

    Fixed #76

--- contracts/piMarket.sol
+++ contracts/piMarket.sol
@@ -122,7 +122,7 @@ contract piMarket is ERC721Holder, ReentrancyGuard {
         emit SaleCreated(_tokenId, _contractAddress, _saleIdCounter.current());
     }
 
-        function editSalePrice(uint256 _saleId, uint256 _price) public {
+    function editSalePrice(uint256 _saleId, uint256 _price) public {
         require(
             msg.sender == _tokenMeta[_saleId].currentOwner,
             "You are not the owner"
