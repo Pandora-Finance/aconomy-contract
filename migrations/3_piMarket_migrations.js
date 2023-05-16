3c3d671d15aa6cbe0de4323647739055e3d2434a
srsydv <shrish@iiitmanipur.ac.in>
2023-05-16 14:49:27 +0530

    Fixed #122 migration

--- migrations/3_piMarket_migrations.js
+++ migrations/3_piMarket_migrations.js
@@ -9,11 +9,7 @@ module.exports = async function (deployer) {
   await deployer.deploy(LibMarket);
   await deployer.link(LibMarket, [PiMarket])
 
-  await deployer.deploy(CollectionMethods);
-  var CollectionMethod = await CollectionMethods.deployed();
-
-  await deployer.deploy(CollectionFactory, CollectionMethod.address);
-  var collectionFactory = await CollectionFactory.deployed();
+  let collectionFactory = await CollectionFactory.deployed();
 
   await deployer.deploy(PiMarket, "0xFF708C09221d5BA90eA3e3A3C42E2aBc8cA8aAc9", collectionFactory.address);
   let market = await PiMarket.deployed();
