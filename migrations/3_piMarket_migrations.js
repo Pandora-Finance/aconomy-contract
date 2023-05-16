9f307417c8de823388511799a686bf356e6285ac
srsydv <shrish@iiitmanipur.ac.in>
2023-05-16 14:43:01 +0530

    Fixed #122 migration

--- migrations/3_piMarket_migrations.js
+++ migrations/3_piMarket_migrations.js
@@ -1,12 +1,21 @@
 const PiMarket = artifacts.require("piMarket");
 const LibMarket = artifacts.require("LibMarket");
+
+const CollectionFactory = artifacts.require("CollectionFactory")
+const CollectionMethods = artifacts.require("CollectionMethods")
 // require("dotenv").config();
 
 module.exports = async function (deployer) {
   await deployer.deploy(LibMarket);
   await deployer.link(LibMarket, [PiMarket])
 
-  await deployer.deploy(PiMarket, "0xFF708C09221d5BA90eA3e3A3C42E2aBc8cA8aAc9");
+  await deployer.deploy(CollectionMethods);
+  var CollectionMethod = await CollectionMethods.deployed();
+
+  await deployer.deploy(CollectionFactory, CollectionMethod.address);
+  var collectionFactory = await CollectionFactory.deployed();
+
+  await deployer.deploy(PiMarket, "0xFF708C09221d5BA90eA3e3A3C42E2aBc8cA8aAc9", collectionFactory.address);
   let market = await PiMarket.deployed();
   console.log("market:", market.address);
 };
