1a93ea16df7b58d7121e32aae4fac599b02e2842
srsydv <shrish@iiitmanipur.ac.in>
2023-09-23 16:08:28 +0530

    piMarket tests

--- test/piMarket.js
+++ test/piMarket.js
@@ -102,7 +102,7 @@ describe("piMarket", function () {
         LibMarket: await LibMarket.getAddress(),
       },
     });
-    const piMarket = await upgrades.deployProxy(
+    piMarket = await upgrades.deployProxy(
       pimarket,
       [
         await aconomyFee.getAddress(),
@@ -211,5 +211,152 @@ describe("piMarket", function () {
       );
       expect(commission.commission.value).to.equal(1000);
     });
+
+
+    it("should let carl transfer piNFT to alice", async () => {
+      await piNFT.connect(carl).safeTransferFrom(carl.getAddress(), alice.getAddress(), 0);
+      expect(await piNFT.ownerOf(0)).to.equal(await alice.getAddress());
+    });
+
+    it("should not put on sale if the contract is paused", async () => {
+      await piMarket.pause();
+
+      await expect(
+        piMarket.sellNFT(
+          piNFT.getAddress(),
+          0,
+          50000,
+          "0x0000000000000000000000000000000000000000"
+        )
+      ).to.be.revertedWith("Pausable: paused");
+      await piMarket.unpause();
+    });
+
+    it("should not place nft on sale if price < 10000", async () => {
+      await piNFT.approve(piMarket.getAddress(), 0);
+      await expect(
+        piMarket.sellNFT(
+          piNFT.getAddress(),
+          0,
+          100,
+          "0x0000000000000000000000000000000000000000"
+        )
+      ).to.be.revertedWithoutReason();
+    })
+
+    it("should not place nft on sale if contract address is 0", async () => {
+      await piNFT.approve(piMarket.getAddress(), 0);
+      await expect(
+        piMarket.sellNFT(
+          "0x0000000000000000000000000000000000000000",
+        0,
+        50000,
+        "0x0000000000000000000000000000000000000000"
+        )
+      ).to.be.revertedWithoutReason();
+    })
+
+    it("should let alice place piNFT on sale", async () => {
+      await piNFT.approve(piMarket.getAddress(), 0);
+      const result = await piMarket.sellNFT(
+        piNFT.getAddress(),
+        0,
+        50000,
+        "0x0000000000000000000000000000000000000000"
+      );
+      expect(await piNFT.ownerOf(0)).to.equal(await piMarket.getAddress());
+    });
+
+
+    it("should edit the price after listing on sale", async () => {
+      await piMarket.connect(alice).editSalePrice(1, 60000);
+      await expect(
+        piMarket.connect(bob).editSalePrice(1, 60000)
+      ).to.be.revertedWith("You are not the owner");
+
+      await expect(
+        piMarket.connect(alice).editSalePrice(1, 60)
+      ).to.be.revertedWithoutReason();
+      let meta = await piMarket._tokenMeta(1);
+      let price = meta.price;
+      expect(price).to.equal(60000);
+      await piMarket.connect(alice).editSalePrice(1, 50000);
+      let newmeta = await piMarket._tokenMeta(1);
+      expect(newmeta.price).to.equal(50000);
+    });
+
+    it("should not let seller buy their own nft", async () => {
+      await expect(
+        piMarket.connect(alice).BuyNFT(1, false, {  value: 50000 })
+      ).to.be.revertedWithoutReason();
+    })
+
+    it("should let bob buy piNFT", async () => {
+      let meta = await piMarket._tokenMeta(1);
+      expect(meta.status).to.equal(true);
+      // assert.equal(meta.status, true);
+      alice.getBalance();
+
+      // let _balance1 = await alice.getBalance();
+      const contractBalance = await ethers.provider.getBalance(alice.getAddress())
+      // let _balance2 = await royaltyReceiver.getBalance();
+      // let _balance3 = await feeReceiver.getBalance();
+      // let _balance4 = await validator.getBalance();
+
+      result2 = await piMarket.connect(bob).BuyNFT(1, false, { value: 50000 });
+      // console.log(result2.receipt.rawLogs)
+      // assert.equal(await piNFT.ownerOf(0), bob);
+
+      // //validator 200
+      // //royalties 500
+      // //fee 50
+
+      // let balance1 = await web3.eth.getBalance(alice);
+      // let balance2 = await web3.eth.getBalance(royaltyReceiver);
+      // let balance3 = await web3.eth.getBalance(feeReceiver);
+      // let balance4 = await web3.eth.getBalance(validator);
+      // let temp = BigNumber(balance1).minus(BigNumber(_balance1));
+      // // console.log(balance1, " ", _balance1, " ", temp.toString());
+      // assert.equal(
+      //   BigNumber(balance1).minus(BigNumber(_balance1)),
+      //   (50000 * 8200) / 10000,
+      //   "Failed to transfer NFT amount"
+      // );
+
+      // assert.equal(
+      //   BigNumber(balance2).minus(BigNumber(_balance2)),
+      //   (50000 * 500) / 10000,
+      //   "Failed to transfer royalty amount"
+      // );
+
+      // // console.log(Number(web3.utils.toBN(balance2)-(web3.utils.toBN(_balance2))))
+
+      // // console.log(Number(BigNumber(balance3).minus(BigNumber(_balance3))));
+      // assert.equal(
+      //   BigNumber(balance3).minus(BigNumber(_balance3)),
+      //   (50000 * 100) / 10000,
+      //   "Failed to transfer fee amount"
+      // );
+
+      // assert.equal(
+      //   BigNumber(balance4).minus(BigNumber(_balance4)),
+      //   (50000 * 1200) / 10000,
+      //   "Failed to transfer validator amount"
+      // );
+
+      // meta = await piMarket._tokenMeta(1);
+      // assert.equal(meta.status, false);
+      // let commission = await piNftMethods.validatorCommissions(piNFT.address, 0);
+      // assert(commission.isValid == false);
+      // assert(commission.commission.account == validator);
+      // assert(commission.commission.value == 1000);
+    });
+
+
+
+
+
+
+
   });
 });
