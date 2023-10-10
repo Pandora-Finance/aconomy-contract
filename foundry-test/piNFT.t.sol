a9b967f1ba527e882b86a10a80da893b7e71dd98
Adya1999 <itsadyayadav@gmail.com>
2023-10-10 12:21:23 +0530

    changes

---
+++ foundry-test/piNFT.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+import "forge-std/Test.sol";
+import {TestUUPS} from "../src/UUPS.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "forge-std/console.sol";
+import "contracts/piNFT.sol";
+import "contracts/AconomyERC2771Context.sol";
+import "contracts/utils/LibShare.sol";
+import "contracts/piNFTMethods.sol";
+
+contract piNFTTest is Test {
+
+piNFT PINFT;
+piNFTMethods PiNFTMethods;
+
+
+}
