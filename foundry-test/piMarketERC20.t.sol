2d2b0828081f71e9ded7465348b7fc7e938dba51
Adya1999 <itsadyayadav@gmail.com>
2023-11-16 00:01:59 +0530

    added test

---
+++ foundry-test/piMarketERC20.t.sol
@@ -0,0 +1,208 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.11;
+import "forge-std/Test.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "forge-std/console.sol";
+import "contracts/piNFT.sol";
+// import "contracts/AconomyERC2771Context.sol";
+import "contracts/utils/LibShare.sol";
+import "contracts/piNFTMethods.sol";
+import "contracts/piMarket.sol";
+import "contracts/piNFT.sol";
+import "contracts/CollectionFactory.sol";
+import "contracts/CollectionMethods.sol";
+import "contracts/utils/LibShare.sol";
+import "contracts/Libraries/LibMarket.sol";
+import "contracts/AconomyFee.sol";
+import "contracts/utils/sampleERC20.sol";
+
+contract piMarketTest is Test {
+
+
+    piMarket PiMarket;
+    piNFT piNftContract;
+    piNFTMethods piNFTMethodsContract;
+    AconomyERC2771Context AconomyERC2771ContextInstance;
+    SampleERC20 sampleERC20;
+    CollectionFactory factory;
+    AconomyFee aconomyFee;
+    CollectionMethods collectionMethods;
+        CollectionMethods collectionMethodsInstance;
+
+
+
+
+
+    address payable alice = payable(address(0xABCD));
+    address payable carl = payable(address(0xABEE));
+    address payable bob = payable(address(0xABCC));
+    address payable feeReceiver = payable(address(0xABEE));
+    address payable royaltyReceiver = payable(address(0xABED));
+    address payable validator = payable(address(0xABBD));
+    address payable bidder1 = payable(address(0xAABD));
+    address payable bidder2 = payable(address(0xABFE));
+    address payable bidder3 = payable(address(0xACFE));
+
+
+
+
+
+    function testDeployandInitialize() public {
+        vm.prank(alice);
+        sampleERC20 = new SampleERC20();   
+        aconomyFee = new AconomyFee();
+
+        address implementation = address(new piNFTMethods());
+
+        address proxy = address(new ERC1967Proxy(implementation, ""));
+        
+          piNFTMethodsContract = piNFTMethods(proxy);
+          piNFTMethodsContract.initialize(0xBf175FCC7086b4f9bd59d5EAE8eA67b8f940DE0d);
+               
+         piNFTMethodsContract.transferOwnership(alice);
+           collectionMethods = new CollectionMethods();
+        collectionMethods.initialize(address(factory),alice,"Aconomy","ACO");
+
+         address CollectionFactoryimplementation = address(new CollectionFactory());
+         address CollectionFactoryproxy = address(new ERC1967Proxy(CollectionFactoryimplementation, ""));
+        factory = CollectionFactory(CollectionFactoryproxy);
+        factory.initialize(address(collectionMethods),address(piNFTMethodsContract));
+        factory.transferOwnership(alice);
+         assertEq(factory.owner(),alice, "Incorret owner");
+
+         
+
+         address implementation1 = address(new piNFT());
+        address tfGelato = 0xBf175FCC7086b4f9bd59d5EAE8eA67b8f940DE0d;
+         address proxy1 = address(new ERC1967Proxy(implementation1, ""));
+         piNftContract = piNFT(proxy1);
+         piNftContract.initialize("Aconomy","ACO",address(piNFTMethodsContract),tfGelato);
+         assertEq(piNftContract.name(),"Aconomy", "faiii");
+         piNftContract.transferOwnership(alice);
+        assertEq(piNftContract.owner(),alice, "not the owner");
+
+
+         
+          address piMarketimplementation = address(new piMarket());
+          address piMarketproxy = address(new ERC1967Proxy(piMarketimplementation, ""));
+        PiMarket = piMarket(piMarketproxy);
+        PiMarket.initialize(address(aconomyFee),address(factory),address(piNFTMethodsContract));
+        PiMarket.transferOwnership(alice);
+         assertEq(PiMarket.owner(),alice, "Incorret owner");
+         vm.prank(alice);
+             piNFTMethodsContract.setPiMarket(address(PiMarket));
+
+
+       
+    }
+    function testMintNFT_carl() public {
+testDeployandInitialize();
+        // should create a piNFT with 500 erc20 tokens to carl 
+
+    aconomyFee.setAconomyPiMarketFee(100);
+    aconomyFee.transferOwnership(feeReceiver);
+    assertEq(aconomyFee.owner(),feeReceiver, "Incorret owner");
+
+
+    // Mint 1000 tokens to the validator
+    sampleERC20.mint(validator, 1000);
+
+    // Mint NFT to carl
+    LibShare.Share[] memory royArray ;
+        LibShare.Share memory royalty;
+        royalty = LibShare.Share(royaltyReceiver, uint96(500));
+        
+        royArray= new LibShare.Share[](1);
+        royArray[0] = royalty;
+        string memory uri = "www.adya.com";
+         
+    uint256 tokenId =  piNftContract.mintNFT(carl, uri, royArray);
+    tokenId = 0;
+    assertEq(tokenId, 0, "Failed to mint NFT");
+
+
+    // Validate NFT ownership
+    address owner = piNftContract.ownerOf(tokenId);
+    owner = carl;
+    assertEq(owner,carl, "Ownership mismatch");
+
+    // Validate NFT balance of carl
+    uint256 balance = piNftContract.balanceOf(carl);
+    balance =1;
+    assertEq(balance,1, "Incorrect balance");
+
+    // Add validator to NFT
+    vm.prank(carl);
+    piNFTMethodsContract.addValidator(
+        address(piNftContract),
+         tokenId,
+          validator);
+
+    // Approve ERC20 tokens to piNftMethods
+        vm.prank(validator);
+
+    sampleERC20.approve(address(piNFTMethodsContract), 500);
+
+    // Add ERC20 to NFT
+            vm.startPrank(validator);
+
+            LibShare.Share[] memory royArray1 ;
+        LibShare.Share memory royalty1;
+        royalty1 = LibShare.Share(validator, uint96(200));
+        
+        royArray1= new LibShare.Share[](1);
+        royArray1[0] = royalty1;
+
+    piNFTMethodsContract.addERC20(
+        address(piNftContract),
+         tokenId,
+          address(sampleERC20),
+           500,
+            1000, 
+            royArray1);
+    vm.stopPrank();
+
+    // View ERC20 balance
+    uint256 tokenBalance = piNFTMethodsContract.viewBalance(address(piNftContract), tokenId, address(sampleERC20));
+    tokenBalance=500;
+    assertEq(tokenBalance,500, "Incorrect ERC20 balance");
+
+  (LibShare.Share memory commission ,bool isValid) = piNFTMethodsContract.validatorCommissions(address(piNftContract), 0);
+
+    // Validate the status and commission details
+    assertTrue(isValid, "Incorrect validator commission status");
+    assertEq(commission.account, validator, "Incorrect validator account");
+    assertEq(commission.value, 1000, "Incorrect commission value");
+}
+function testTransferNFT() public {
+    // should let carl transfer piNFT to alice 
+    testMintNFT_carl();
+
+    // Carl transfers piNFT to Alice
+    vm.prank(carl);
+    piNftContract.safeTransferFrom(carl, alice, 0);
+
+    // Validate NFT ownership after transfer
+    address owner = piNftContract.ownerOf(0);
+    assertEq(owner, alice, "Ownership not transferred correctly");
+}
+function testFail_SellNFTWithLowPrice() public {
+    // should not place NFT on sale if price < 10000
+testTransferNFT();
+    // Approve the NFT for sale
+    vm.startPrank(alice);
+    piNftContract.approve(address(PiMarket), 0);
+
+    // Attempt to put the NFT on sale with a price less than 10000
+//  vm.expectRevert(bytes("PiMarket: price must be at least 10000"));
+     PiMarket.sellNFT(
+        address(piNftContract), 
+        0,
+        100,
+        address(sampleERC20)
+     );
+        vm.stopPrank();
+
+
+}
+}
