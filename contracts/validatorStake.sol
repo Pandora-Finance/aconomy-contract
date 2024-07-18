10b6b341ffe5ecee1be6effa473c8dd76caebcfa
Shrish Kumar <shrishofficial18@gmail.com>
2024-07-18 10:46:27 +0530

    4. Redundant variable

--- contracts/validatorStake.sol
+++ contracts/validatorStake.sol
@@ -22,7 +22,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
     struct StakeDetail {
         uint256 stakedAmount;
         uint256 refundedAmount;
-        address ERC20Address;
     }
 
     mapping(address => StakeDetail) public validatorStakes;
